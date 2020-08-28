75290fb0f1fafe0ae1d4b86366db7454f0fec901
Cadé Philippe <XPHC@SWISS-AS.COM>
2020-08-28 15:40:07 +0200

    Skip ComponentRendererTest if running headless

--- src/test/java/de/rosstauscher/comparandum/renderer/ComponentRendererTest.java
+++ src/test/java/de/rosstauscher/comparandum/renderer/ComponentRendererTest.java
@@ -3,9 +3,11 @@ package de.rosstauscher.comparandum.renderer;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.awt.Dimension;
+import java.awt.GraphicsEnvironment;
 
 import javax.swing.JLabel;
 
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 
 import de.rosstauscher.comparandum.junit.Comparandum;
@@ -24,6 +26,7 @@ class ComponentRendererTest {
 	 ************************************************************************/
 	@Test
 	void paintComponentRenderableShouldWork() {
+		Assumptions.assumeFalse(GraphicsEnvironment.isHeadless());
 		JLabel c = new JLabel("Hello World");
 		c.setPreferredSize(new Dimension(80, 20));
 		ComponentRenderable r = new ComponentRenderable(c);
